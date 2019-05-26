baf876fae95cedc33a7ae922a57d12242eea0455
Jahan <jahanulhaque@gmail.com>
2019-05-26 17:41:58 +0100

    tapping adds new text to cells

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,19 @@ import 'package:intl/intl.dart'; // Time package
 
 void main() => runApp(MyApp());
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       title: 'Flutter',
-//       theme: new ThemeData(primarySwatch: Colors.yellow),
-//       home: new MyHomePage(),
-//     );
-//   }
-// }
-
-
-// This is like the TableViewDelegate
-// Also makes a state, which is then added to the view controller?
-class Blocks extends StatefulWidget {
-  // Stateful are mutable / Can change
-  @override
-  BlocksState createState() => BlocksState();
-}
-
-// This is like the TableViewDataSource
-class BlocksState extends State<Blocks> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _buildBlocks();
+    return MaterialApp(
+      // title: 'Welcome to Flutter',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        // body: Center(
+        body: BodyLayout(),
+        // child: Blocks(),
+        // ),
+      ),
+    );
   }
 }
 
@@ -35,16 +23,6 @@ String getDate(index) {
   DateTime date = DateTime.now();
   var prevMonth = new DateTime(date.year, date.month, date.day + index);
   return DateFormat('EEE d MMM').format(prevMonth).toString();
-}
-
-String createArrayElements(i) {
-  // Gets the cells
-  final array = [];
-  // Rounding down basically, if time is 80:30, we want the 8PM Cell to be their
-  for (int i = getCurrentHour(); i < 48; i++) {
-    array.add("Empty");
-  }
-  return array[i];
 }
 
 int getCurrentHour() {
@@ -62,115 +40,196 @@ String getHours(i) {
 
   // Rounding down basically, if time is 80:30, we want the 8PM Cell to be their
   int currentHour = getCurrentHour();
-
   while (currentHour < 48) {
     var y = currentHour + 1; // Store the actual count
     currentHour = currentHour % 24; // keep number between 0 & 24
-
     (y > 24 && y < 37) //
         ? time.add(currentHour.toString() +
             "AM") // if Between midnight hours (12am - 12pm)
         : time.add(currentHour.toString() + "PM"); // Else it is PM
-
     currentHour = y; // Reassign the count
   }
   return time[i];
 }
 
+// This is like the TableViewDelegate
+// Also makes a state, which is then added to the view controller?
+class Blocks extends StatefulWidget {
+  // Stateful are mutable / Can change
+  @override
+  BlocksState createState() => BlocksState();
+}
+
+// This is like the TableViewDataSource
+class BlocksState extends State<Blocks> {
+  @override
+  Widget build(BuildContext context) {
+    // return _buildBlocks(context);
+  }
+}
+
+// -------
+class BodyLayout extends StatefulWidget {
+  @override
+  BodyLayoutState createState() {
+    return new BodyLayoutState();
+  }
+}
+
+List<String> array = [];
+int x = 0;
+
+int makeArray() {
+  if (x < 47) {
+    for (int i = getCurrentHour(); i < 48; i++) {
+      array.add("Empty");
+      x = i;
+    }
+    print("DONE");
+    print(x);
+  }
+}
+
+class BodyLayoutState extends State<BodyLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView();
+  }
+
 // This is like the TableView
-Widget _buildBlocks() {
-  int currentHour = getCurrentHour() - 1 % 24;
-  return ListView.builder(
-      padding: const EdgeInsets.all(32.0),
-      physics: const BouncingScrollPhysics(),
-      itemCount: getArrayLength(),
-      itemBuilder: (context, index) {
-        currentHour += 1;
-        print(currentHour);
+  Widget _myListView() {
+    print("hello");
+    makeArray();
 
-        if (index == 0) {
-          return ListTile(
-            title: Text(
-              "Today",
-              style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.left,
-              // style: Theme.of(context).textTheme.headline,
-            ),
-            subtitle: Text(
-              getDate(0),
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w400),
-              textAlign: TextAlign.left,
-            ),
-          );
-          // Just adding this for now, depends how the arrays etc are worked out
-        } else if (currentHour == 24) {
-          return ListTile(
-            title: Text(
-              "Tomorrow",
-              style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.left,
-            ),
-            subtitle: Text(
-              getDate(1), // Increments the day
-              style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w400),
-              textAlign: TextAlign.left,
-            ),
-          );
-        }
-        return _buildCell(index);
-      });
-}
+    int currentHour = getCurrentHour() - 1 % 24;
+    // Makes the cells
 
-// This is like the TableViewCell
-Widget _buildCell(int i) {
-  return Container(
-    padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-    child: Card(
-      color: Colors.white,
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(24, 24, 24, 24),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    getHours(
-                        i), // Will be changed into a variable/array for every hour, use the array index or a seperate for loop to do it
-                    style: TextStyle(fontSize: 14, color: Colors.grey),
-                    textAlign: TextAlign.left,
-                  ),
-                  Text(
-                    createArrayElements(i), // ^ similar to time
-                    style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey),
-                    textAlign: TextAlign.left,
-                  ),
-                ],
+    return ListView.builder(
+        padding: const EdgeInsets.all(32.0),
+        physics: const BouncingScrollPhysics(),
+        itemCount: getArrayLength(),
+        itemBuilder: (context, index) {
+          currentHour += 1;
+          if (index == 0) {
+            return ListTile(
+              onTap: () {
+                print('Moon');
+              },
+              title: Text(
+                "Today",
+                style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left,
+                // style: Theme.of(context).textTheme.headline,
               ),
+              subtitle: Text(
+                getDate(0),
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400),
+                textAlign: TextAlign.left,
+              ),
+            );
+            // Just adding this for now, depends how the arrays etc are worked out
+          } else if (currentHour == 24) {
+            return ListTile(
+              title: Text(
+                "Tomorrow",
+                style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left,
+              ),
+              subtitle: Text(
+                getDate(1), // Increments the day
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400),
+                textAlign: TextAlign.left,
+              ),
+            );
+          }
+          return Card(
+            child: ListTile(
+              title: Text(
+                getHours(
+                    index), // Will be changed into a variable/array for every hour, use the array index or a seperate for loop to do it
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+                textAlign: TextAlign.left,
+              ),
+              subtitle: Text(
+                // "ee",
+                array[index],
+                style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+                textAlign: TextAlign.left,
+              ),
+              onTap: () {
+                //                                  <-- onTap
+                setState(() {
+                  array[index] = "test";
+                  print(array);
+                  print(array.length);
+                });
+              },
+              // onLongPress: () {
+              //   //                            <-- onLongPress
+              //   setState(() {
+              //     array.removeAt(index);
+              //   });
+              // },
             ),
-            Icon(Icons.add_circle, size: 48, color: Colors.amberAccent),
-          ],
-        ),
-      ),
-    ),
-  );
+          );
+
+          // return _buildCell(index);
+        });
+  }
 }
+// This is like the TableViewCell
+// Widget _buildCell(int i) {
+//   return Container(
+//     padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+//     child: Card(
+//       color: Colors.white,
+//       elevation: 4.0,
+//       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+//       child: Padding(
+//         padding: const EdgeInsets.fromLTRB(24, 24, 24, 24),
+//         child: Row(
+//           children: [
+//             Expanded(
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.stretch,
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Text(
+//                     getHours(
+//                         i), // Will be changed into a variable/array for every hour, use the array index or a seperate for loop to do it
+//                     style: TextStyle(fontSize: 14, color: Colors.grey),
+//                     textAlign: TextAlign.left,
+//                   ),
+//                   Text(
+//                     createArrayElements(i), // ^ similar to time
+//                     style: TextStyle(
+//                         fontSize: 24.0,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.grey),
+//                     textAlign: TextAlign.left,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Icon(Icons.add_circle, size: 48, color: Colors.amberAccent),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
